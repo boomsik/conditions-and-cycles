e06f41cc344d50e55460e5497c1224f13bf1b75e
Viktor <pozdnyakov151994@gmail.com>
2022-10-02 20:27:17 +0300

    first lesson

---
+++ script.js
@@ -0,0 +1,20 @@
+var name = "Николай";
+console.log("Привет, " + name);
+if (name.length > 6) {
+ console.log("Ну и длиннющее же у вас имя!");
+} else {
+ console.log("Имя у вас не из длинных.");
+}
+
+var lemonChicken = false;
+var beefWithBlackBean = true;
+var sweetAndSourPork = true;
+if (lemonChicken) {
+ console.log("Отлично! Я буду курицу с лимоном!");
+} else if (beefWithBlackBean) {
+ console.log("Заказываю говядину.");
+} else if (sweetAndSourPork) {
+ console.log("Ладно, закажу свинину.");
+} else {
+ console.log("Что ж, остается рис с яйцом.");
+}
